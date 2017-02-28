78c5b22ee878ac559e15cd30f6df5a6a5485b868
Toby Bradshaw <tobybrad@microsoft.com>
2017-02-28 11:41:37 +0000

    Deal with 500's from MO

--- index.js
+++ index.js
@@ -16,12 +16,16 @@ function askMO(location){
             method: 'GET'
         };
         request(options, (err, response, body) => {
-            resolve(JSON.parse(body));
+            if (response.statusCode == 200) {
+                resolve(JSON.parse(body));
+            } else {
+                resolve(body);
+            }
         })
     })
 }
 
-function askLuis(appId, subKey, q) {
+function _askLUIS(appId, subKey, q) {
     var uri = `https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/${appId}?subscription-key=${subKey}&verbose=true&q=${q}`;
 
     return new Promise((resolve, reject) => {
@@ -35,8 +39,13 @@ function askLuis(appId, subKey, q) {
     })
 }
 
+function askLUIS(q) {
+	return _askLUIS(config.get("APP_ID"), config.get("SUB_KEY"), q);
+}
+
 function main() {
 
+    // Set up the bot server..
     var server = restify.createServer();
     server.use(restify.bodyParser({mapParams: false}));
     server.listen(process.env.port || process.env.PORT || 3978, function () {
@@ -47,54 +56,59 @@ function main() {
         appId: process.env.MICROSOFT_APP_ID,
         appPassword: process.env.MICROSOFT_APP_PASSWORD
     });
-
     server.post('/api/messages', connector.listen());
 
     var bot = new builder.UniversalBot(connector);
 
+    // Root dialog 
     bot.dialog('/', [
-        (session, args) => {
-            askLuis(config.get("APP_ID"), config.get("SUB_KEY"), session.message.text)
+        (session) => {
+			// Ask 
+            askLUIS(session.message.text)
             .then((response) => {
                 switch(response.topScoringIntent.intent) {
                     case("None") :
                         session.send("Sorry I don't know what your getting at!");
                         return;
                     case("getForecast") :
-                        session.beginDialog("/getForecast", response.entities)
+                	    session.beginDialog("/getForecast", response.entities)
             };
         });
     }]);
 
     bot.dialog('/getForecast', [
+		// Get forecast, we need a location.
+		// Caller may have passed us location in entities 
         (session, args, next) => {
             if (!args || args.length == 0) {
+				// Don't have a location, ask for one
                 session.beginDialog("/getLocation");
             } else {
+				// Looks like we have a location, go to next step
+				// we're faking up a response object to keep all
+				// the calls simple
                 next({response:args[0].entity});
             }
         }, 
         (session, args, next) => {
-            // session.userData.location = session.message.text;
+			// args.response 
             askMO(args.response)
-                .then((response)=>{
+            .then((response)=>{
+                if (typeof response === "object") {
                     session.send(response.properties.forecast.text.local);
-                    session.endDialog();
-                }); 
+                }
+                else {
+                    session.send(response);
+                }
+                session.endDialog();
+            }); 
         }
     ]);
     
     bot.dialog('/getLocation', [
+		// Ask the user what location they were thinking of
         (session, args, next) => {
-            if(!args || args.length == 0) {
-                if(!session.userData.location){
-                    builder.Prompts.text(session, "Where?"); 
-                } else {
-                    session.endDialog();
-                }
-            } else {
-                session.endDialog();
-            }
+ 	       builder.Prompts.text(session, "Where?"); 
         }, 
         (session, results) => {
             session.endDialogWithResult(results);
